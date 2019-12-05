b4ed118fc9c677c18d986a764bec528261960148
kafaichan <kafaichan1201@gmail.com>
2019-12-05 21:09:44 +0800

    fix style not applied to card problem

--- Card.js
+++ Card.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {
-  SafeAreaView,
-  StatusBar,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native'
+    SafeAreaView,
+    StatusBar,
+    View, Text, StyleSheet, 
+    TouchableOpacity,
+    Dimensions,
+  } from 'react-native'
 
-class Card extends Component {
-  render() {
-    return (
-      <TouchableOpacity style={{ ...this.props.style }} onPress={ this.props.onPress }>
-        <Text style={{ fontSize: this.props.fontSize || 32 }}>
-          { this.props.isShow ? this.props.title : this.props.cover }
-        </Text>
-      </TouchableOpacity>
-    )
+  class Card extends Component {
+      render() {
+          return (
+              <TouchableOpacity style={{ ...this.props.style }} onPress={ this.props.onPress }>
+                <Text style="{{fontSize: this.props.fontSize || 32}}">{this.props.isShow ? this.props.title : this.props.cover}</Text>
+              </TouchableOpacity>
+          )
+      }
   }
-}
 
-export default Card 
+  export default Card
